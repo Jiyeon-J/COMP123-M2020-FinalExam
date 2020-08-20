f7c9b60e261248780aa3a159f1949926d3e03f78
Jiyeon Jeon <jiyeon5168@gmail.com>
2020-08-19 22:47:34 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/GenerateNameForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public partial class GenerateNameForm : Form
+    {
+        public GenerateNameForm()
+        {
+            InitializeComponent();
+        }
+
+        public void GenerateNames()
+        {
+
+        }
+        private void GenerateNameForm_Load(object sender, EventArgs e)
+        {
+            GenerateNames();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            GenerateNames();
+
+            FirstNameTextBox.Text = Program.character.Firstname;
+            LastNameTextBox.Text = Program.character.Lastname;
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            AbilityGeneratorForm abilityGeneratorForm = new AbilityGeneratorForm;
+            this.Hide();
+            abilityGeneratorForm.Show();
+        }
+
+        private void FirstNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LastNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ COMP123-S2016-FinalExam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public static class Program
+    {
+        public static Character character = new Character();
+        
+        public static SplashForm splashForm;
+        public static GenerateNameForm generateNameForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            splashForm = new SplashForm();
+            generateNameForm = new GenerateNameForm();
+            
+            Application.Run(new AbilityGeneratorForm());
+        }
+    }
+}
